bb6fd06bffdfc8d91c7e599b7a3a2c5ea8d26f0e
Bruno P.M <brunoporras2001@gmail.com>
2023-01-03 06:44:22 -0500

    Flujo de envío ok

--- routes.js
+++ routes.js
@@ -3,39 +3,48 @@ import nodemailer from "nodemailer";
 
 const router = Router();
 
-router.get("/sendConfirmacion", (req, res) => {
-  const { esp, medico, dia, email } = req.body;
+router.post("/sendConfirmacion", (req, res) => {
+  const { esp, medico, email } = req.body;
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.user,
-      pass: process.env.pass,
-    },
-  });
+  if (email == undefined || medico == undefined || esp == undefined) {
+    res.json({
+      message: "Fail",
+      detail: "Datos insuficientes para el envío.",
+    });
+  } else {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.user,
+        pass: process.env.pass,
+      },
+    });
 
-  let mailOptions = {
-    from: process.env.user, // TODO: email sender
-    to: email, // TODO: email receiver
-    subject: "Confirmación de cita",
-    text:
-      "¡Gracias por usar nuestra app!\nCita reservada para el día " +
-      dia +
-      " con el Dr." +
-      medico +
-      ", especialista en " +
-      esp,
-  };
+    let mailOptions = {
+      from: process.env.user, // TODO: email sender
+      to: email, // TODO: email receiver
+      subject: "Confirmación de cita",
+      text:
+        "¡Gracias por usar nuestra app!\nCita reservada con el Dr." +
+        medico +
+        ", especialista en " +
+        esp +
+        ". Para más detalles comunicarse con el soporte de la aplicación.",
+    };
 
-  transporter.sendMail(mailOptions, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.send("FAIL :(");
-    }
-    res.send("SENT IT!!!");
-  });
+    transporter.sendMail(mailOptions, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.json({
+          message: "Fail",
+          detail: "Error al enviar el correo a " + email,
+        });
+      }
+      res.json({ message: "Success", detail: "Correo enviado" });
+    });
+  }
 });
 
 export default router;
